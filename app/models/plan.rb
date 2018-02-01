a287dfc3db6310e3e848399d7eaaee710c0aed29
RasPhilCo <RasPhilCo@users.noreply.github.com>
2018-01-31 23:47:04 -0800

    rebrand web app

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,10 +2,10 @@ class Plan
   include ActiveModel::Serialization
 
   PLANS = [
-    { id: "basic", price: 0, range: 0..0, title: "Hound" },
-    { id: "tier1", price: 49, range: 1..4, title: "Chihuahua" },
-    { id: "tier2", price: 99, range: 5..10, title: "Labrador" },
-    { id: "tier3", price: 249, range: 11..30, title: "Great Dane" },
+    { id: "basic", price: 0, range: 0..0, title: "Open Comber" },
+    { id: "tier1", price: 49, range: 1..4, title: "Lite Comber" },
+    { id: "tier2", price: 99, range: 5..10, title: "Comber" },
+    { id: "tier3", price: 249, range: 11..30, title: "Large Comber" },
   ].freeze
 
   attr_reader :id, :price, :title
