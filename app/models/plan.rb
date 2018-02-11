215f52dde62c11035b1bfc065a808b8fa272c750
RasPhilCo <RasPhilCo@users.noreply.github.com>
2018-02-10 17:25:22 -0800

    update hero section

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -4,8 +4,8 @@ class Plan
   PLANS = [
     { id: "basic", price: 0, range: 0..0, title: "Comb Explorer" },
     { id: "tier1-19", price: 19, range: 1..20, title: "Lite Comber" },
-    { id: "tier2-49", price: 49, range: 21..100, title: "Comber" },
-    { id: "tier3-299", price: 299, range: 101..1000000, title: "Large Comber" }
+    { id: "tier2-49", price: 49, range: 21..100, title: "Large Comber" },
+    { id: "tier3-299", price: 299, range: 101..1000000, title: "XLarge Comber" }
   ].freeze
 
   attr_reader :id, :price, :title
